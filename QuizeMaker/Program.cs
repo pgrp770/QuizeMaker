afd99d563301c3ce144e50d536fc0e84b32cbbaf
pgrp770 <pgrp770@gmail.com>
2024-07-21 12:37:39 +0300

    Add project files.

---
+++ QuizeMaker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+
+namespace QuizeMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Path = "C:\\Users\\pgrp7\\OneDrive\\Desktop\\programming\\week4\\QuizeMaker\\QuizeMaker\\try.xml";
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(Path, System.Text.Encoding.UTF8);
+            xmlTextWriter.Formatting = Formatting.Indented;
+            xmlTextWriter.WriteStartDocument();
+            xmlTextWriter.WriteStartElement("Pini");
+            xmlTextWriter.WriteStartElement("Tzvi");
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.Flush();
+            xmlTextWriter.Close();
+            
+        }
+    }
+}
---
+++ QuizeMaker/Quiz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizeMaker
+{
+    internal class Quiz
+    {
+        public string Quiestion;
+        public string Answer;
+
+        public Quiz(string quiestion, string answer)
+        {
+            Quiestion = quiestion;
+            Answer = answer;
+        }
+
+       
+    }
+}
